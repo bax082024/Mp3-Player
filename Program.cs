0c9181563c3481d4022d75b819f0c1a1bc5c4cd6
bax082024 <bax082024@gmail.com>
2024-10-26 03:49:46 +0200

    added variables

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 using System.IO;
 using NAudio.Wave;
 
+class AudioPlayer
+{
+  private WaveOutEvent outputDevice;
+  private AudioFileReader audioFile;
+  private string[] playlist;
+  private int currentTrackIndex;
+
+  public AudioPlayer(string folderPath) 
+  {
+    playlist = Directory.GetFiles(folderPath, "*.mp3")
+  }
+}
