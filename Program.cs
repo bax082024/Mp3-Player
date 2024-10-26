60fdc120d6ccc5fd5ea24dd095730eb9b8fa517d
bax082024 <bax082024@gmail.com>
2024-10-26 03:52:52 +0200

    added if folder empty function

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 using NAudio.Wave;
 
 class AudioPlayer
@@ -11,6 +12,24 @@ class AudioPlayer
 
   public AudioPlayer(string folderPath) 
   {
-    playlist = Directory.GetFiles(folderPath, "*.mp3")
+    playlist = Directory.GetFiles(folderPath, "*.mp3");
+  }
+
+  public void Play()
+  {
+    if (playlist.Length == 0)
+    {
+      Console.WriteLine("No Mp3 Files Found.");
+      return;
+    }
+
+    PlayTrack(currentTrackIndex);
+
+    private void PlayTrack(int trackIndex)
+    {
+      
+    }
+
+
   }
 }
