2ab4096fa80b96a3dfd92573895432547f5df471
bax082024 <bax082024@gmail.com>
2024-10-26 03:57:00 +0200

    fixed syntax error

--- Program.cs
+++ Program.cs
@@ -24,12 +24,21 @@ class AudioPlayer
     }
 
     PlayTrack(currentTrackIndex);
-
+  }
     private void PlayTrack(int trackIndex)
     {
-      
+      if (outputDevice != null)
+      {
+        outputDevice.Dispose();
+        audioFile.Dispose();
+      }
+
+      audioFile = new AudioFileReader(playlist[trackIndex]);
+      outputDevice = new WaveOutEvent();
+      outputDevice.Init(audioFile);
+      outputDevice.Play();
     }
 
 
-  }
+  
 }
